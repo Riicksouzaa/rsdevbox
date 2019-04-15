5458a99c25b255e550dafac33b4816e503e499c2
Ricardo Filho <souzaariick@gmail.com>
2019-04-15 11:35:13 -0300

    Variaveis de ambiente inseridas a aplicação

--- src/models/File.js
+++ src/models/File.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const File = new mongoose.Schema({
+const File = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     path: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     }
-}, {
+  },
+  {
     timestamps: true,
     toObject: {
-        virtuals: true
+      virtuals: true
     },
     toJSON: {
-        virtuals: true
+      virtuals: true
     }
+  }
+);
+
+File.virtual("url").get(function() {
+  const url = process.env.URL || "http://localhost:3333";
+
+  return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
-File.virtual('url').get(function () {
-    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
-});
-
-module.exports = mongoose.model('File', File);
+module.exports = mongoose.model("File", File);
